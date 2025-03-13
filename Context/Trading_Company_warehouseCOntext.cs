4e789f202102c038ef7c5ea026cde4e58ab86b70
Ahmed Roshdi <81536637+AhmedRoshdi36@users.noreply.github.com>
2025-03-13 17:36:42 +0200

    [Feature] first commit with adding warehouse

---
+++ Context/Trading_Company_warehouseCOntext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFcore_project.Context
+{
+    class Trading_Company_warehouseCOntext : DbContext
+    {
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Trading_Company;Integrated Security=True;TrustServerCertificate=True;");
+    }
+}
